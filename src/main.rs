11b4617542b6998e44fcb66acbee7b919d68de1f
crypt0summer <realapril25@gmail.com>
2023-06-19 15:05:00 +0900

    string literal, enum, option enum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080".to_string());
+    let server = Server::new("127.0.0.1:8080".to_string());//string literal vs string
     server.run();
 }
 
@@ -18,3 +18,28 @@ impl Server{
         println!("Listening on {}", self.addr);
     }
 }
+
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+ */
+
+struct Request {
+    path: String,
+    query_string: Option<String>, //Rust doesn't support "No value" - use Option enum
+    method: Method,
+}
+
+enum Method { //Enums
+    GET, 
+    POST, 
+    PUT, 
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
+
