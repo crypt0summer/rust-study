58aad08405e27ef27ee1d653c3707ec1c3aa51f6
crypt0summer <realapril25@gmail.com>
2023-06-19 15:24:17 +0900

    modularize structs

---
+++ src/http/method.rs
@@ -0,0 +1,11 @@
+pub enum Method { //Enums
+    GET, 
+    POST, 
+    PUT, 
+    DELETE,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,15 @@
+use server::Server;
+use http::Request;
+use http::Method; //same as http::method::Method;
+
+mod server; //Relative path
+mod http;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());//string literal vs string
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
 
-impl Server{
-    fn new(addr: String) -> Self {
-        Self {
-            addr
-        }
-    }
 
-    fn run(self) {
-        println!("Listening on {}", self.addr);
-    }
-}
-
-/*
-GET /user?id=10 HTTP/1.1\r\n
-HEADERS \r\n
-BODY
- */
-
-struct Request {
-    path: String,
-    query_string: Option<String>, //Rust doesn't support "No value" - use Option enum
-    method: Method,
-}
-
-enum Method { //Enums
-    GET, 
-    POST, 
-    PUT, 
-    DELETE,
-    HEAD,
-    CONNECT,
-    OPTIONS,
-    TRACE,
-    PATCH,
-}
 
